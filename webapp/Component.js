bd695c243f9a185b37789335f83d843efd26c1b0
Daviti Oghiashvili <daviti.oghiashvili@leverx.com>
2025-07-23 12:17:06 +0400

    added toast button plugin

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,98 +1,97 @@
 sap.ui.define([
 	"sap/ui/core/Component",
-	"sap/m/Button",
+	"sap/ushell/ui/shell/ShellHeadItem",
 	"sap/m/MessageToast",
 	"sap/ui/core/Fragment"
-], function (Component, Button, MessageToast, Fragment) {
+], function (Component, ShellHeadItem, MessageToast, Fragment) {
+	"use strict";
 
 	return Component.extend("plugin.Component", {
-
+		_oDialog: null,
 		metadata: {
-			manifest: "json",
+			manifest: "json"
 		},
 
+		/**
+		 * Initializes the component and adds buttons to the FLP shell header.
+		 */
 		init: function () {
-			let rendererPromise = this._getRendererAsync();
-			let oResourceBundle = this.getModel("i18n").getResourceBundle();
+			const oResourceBundle = this.getModel("i18n").getResourceBundle();
 
-			rendererPromise.then(function (oRenderer) {
-				// Add Toast Button
-				let oToastButton = new Button({
-					text: oResourceBundle.getText("toastButtonText", "Show Toast"),
-					press: function () {
-						MessageToast.show("Hello from the plugin!");
-					}
-				});
-
-				// Add Fragment Button
-				let oFragmentButton = new Button({
-					text: oResourceBundle.getText("fragmentButtonText", "Show Dialog"),
-					press: this._openFragmentDialog.bind(this)
-				});
-
-				// Extend shell header with buttons
-				oRenderer.addHeaderEndItem("sapUshellShellHeadItem", {
+			this._getRendererAsync().then(oRenderer => {
+				// Toast Button
+				const oToastButton = new ShellHeadItem({
 					id: "toastButton",
-					control: oToastButton
-				}, true, false);
+					icon: "sap-icon://alert",
+					text: oResourceBundle.getText("toastButtonText"),
+					press: () => MessageToast.show("Hello from the plugin! Please open the app to see the info")
+				});
 
-				oRenderer.addHeaderEndItem("sapUshellShellHeadItem", {
+				// Open Fragment Button
+				const oFragmentButton = new ShellHeadItem({
 					id: "fragmentButton",
-					control: oFragmentButton
-				}, true, false);
-			}.bind(this));
+					icon: "sap-icon://popup-window",
+					text: oResourceBundle.getText("fragmentButtonText"),
+					press: () => this._openFragmentDialog()
+				});
+
+				// Add buttons to the shell header
+				oRenderer.addHeaderEndItem("sapUshellShellHeadItem", { id: "toastButton", control: oToastButton }, true, false);
+				oRenderer.addHeaderEndItem("sapUshellShellHeadItem", { id: "fragmentButton", control: oFragmentButton }, true, false);
+			}).catch(oError => MessageToast.show("Error initializing plugin", oError));
 		},
 
-		_openFragmentDialog: function () {
-			if (!this._oDialog) {
-				Fragment.load({
-					name: "plugin.view.fragments.Dialog",
-					controller: this
-				}).then(function (oDialog) {
-					this._oDialog = oDialog;
-					this._oDialog.open();
-				}.bind(this));
-			} else {
-				this._oDialog.open();
+		/**
+		 * Opens the dialog fragment, loading it if not already loaded.
+		 */
+		async _openFragmentDialog() {
+			this._oDialog ??= await Fragment.load({
+				name: "plugin.view.fragments.Dialog",
+				controller: this
+			});
+			this._oDialog.open();
+			return this._oDialog;
+		},
+
+		/**
+		 * Closes the dialog.
+		 */
+		onCloseDialog: function () {
+			if (this._oDialog) {
+				this._oDialog.close();
 			}
 		},
 
-		onCloseDialog: function () {
-			this._oDialog.close();
-		},
-
-       /**
-       * @private
-       * @return {Promise<sap.ushell.renderers.fiori2.Renderer>}
-       */
-		_getRendererAsync() {
+		/**
+		 * Retrieves the FLP renderer asynchronously.
+		 * @private
+		 * @returns {Promise<sap.ushell.renderers.fiori2.Renderer>}
+		 */
+		_getRendererAsync: function () {
 			return new Promise((fnResolve, fnReject) => {
-				const vShell = sap.ushell?.Container
+				const vShell = sap.ushell?.Container;
 
 				if (!vShell) {
-					fnReject(
-						'Illegal state: shell container not available; this component must be executed in a unified shell runtime context.'
-					)
+					fnReject("Illegal state: shell container not available.");
+					return;
 				}
 
-				const vRenderer = vShell.getRenderer()
+				const vRenderer = vShell.getRenderer();
 
 				if (vRenderer) {
-					return fnResolve(vRenderer)
+					fnResolve(vRenderer);
+					return;
 				}
 
 				vShell.attachRendererCreatedEvent((oEvent) => {
-					const vCreatedRenderer = oEvent.getParameter('renderer')
-
+					const vCreatedRenderer = oEvent.getParameter("renderer");
 					if (vCreatedRenderer) {
-						fnResolve(vCreatedRenderer)
+						fnResolve(vCreatedRenderer);
+					} else {
+						fnReject("Illegal state: shell renderer not available.");
 					}
-
-					fnReject(
-						"Illegal state: shell renderer not available after receiving 'rendererLoaded' event."
-					)
-				})
-			})
-		},
+				});
+			});
+		}
 	});
 });
